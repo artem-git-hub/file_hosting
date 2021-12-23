4a3c00cb8e62cbb2b5f95b422c28c183309622c2
Artem Arehov <pro100adresrus@gmail.com>
2021-12-23 20:45:27 +0300

    Add reconnect to db from db.py, if error

--- db.py
+++ db.py
@@ -10,6 +10,7 @@ cursor = db.cursor()
 
 
 async def select_db(whatis="*", fromis="users", whereis=''):
+    global db, cursor
     try:
         if whereis == "":
             cursor.execute("""SELECT {} FROM {} ORDER BY id ASC;""".format(whatis, fromis))
@@ -18,12 +19,21 @@ async def select_db(whatis="*", fromis="users", whereis=''):
                 """SELECT {} FROM {} WHERE {} ORDER BY id ASC;""".format(whatis, fromis, whereis))
         return cursor.fetchall()
     except Exception as e:
+        db.close()
+        db = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name
+        )
+        cursor = db.cursor()
         print("+++++++++++++++++++++++++++ОТЧЁТ ОБ ОШИБКЕ+++++++++++++++++\n\n--------------Ошибка в блоке select -----------\n\n\n\nЗапрос:\n\n")
         print("""SELECT {} FROM {} WHERE {} ORDER BY id ASC;""".format(whatis, fromis, whereis))
         print(f"ОШИБКА: \n\n{e}\n\n\n_______________________________КОНЕЦ ОТЧЁТА __________________________________")
         return []
 
 async def insert_db(name_table, column, values):
+    global db, cursor
     try:
         if None in values:
             val = list(values)
@@ -36,6 +46,14 @@ async def insert_db(name_table, column, values):
         )
         db.commit()
     except Exception as e:
+        db.close()
+        db = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name
+        )
+        cursor = db.cursor()
         print("+++++++++++++++++++++++++++ОТЧЁТ ОБ ОШИБКЕ++++++++++++++++++++++++")
         print(f"""INSERT INTO {name_table} {column} VALUES{values}""")
         print(f"ОШИБКА: \n\n{e}\n\n\n_____________________________")
@@ -43,12 +61,21 @@ async def insert_db(name_table, column, values):
 
     
 async def delete_db(name_table, where):
+    global db, cursor
     try:
         # print(f"""-- DELETE FROM {name_table} WHERE {where};""")
         cursor.execute(
             f"""DELETE FROM {name_table} WHERE {where};""")
         db.commit()
     except Exception as e:
+        db.close()
+        db = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name
+        )
+        cursor = db.cursor()
         print("+++++++++++++++++++++++++++ОТЧЁТ ОБ ОШИБКЕ+++++++++++++++++\n\n--------------Ошибка в блоке delete -----------\n\n\n\nЗапрос:\n\n")
         print(f"""-- DELETE FROM {name_table} WHERE {where};""")
         print(f"ОШИБКА: \n\n{e}\n\n\n_______________________________КОНЕЦ ОТЧЁТА __________________________________")
@@ -56,12 +83,21 @@ async def delete_db(name_table, where):
 
 
 async def update_db(name_table, column, value, whereis):
+    global db, cursor
     try:
         # print(f"""UPDATE {name_table} SET {column} = {value} WHERE {whereis};""")
         cursor.execute(
             f"""UPDATE {name_table} SET {column} = {value} WHERE {whereis};""")
         db.commit()
     except Exception as e:
+        db.close()
+        db = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name
+        )
+        cursor = db.cursor()
         print("+++++++++++++++++++++++++++ОТЧЁТ ОБ ОШИБКЕ+++++++++++++++++\n\n--------------Ошибка в блоке update -----------\n\n\n\nЗапрос:\n\n")
         print(f"""UPDATE {name_table} SET {column} = {value} WHERE {whereis};""")
         print(f"ОШИБКА: \n\n{e}\n\n\n_______________________________КОНЕЦ ОТЧЁТА __________________________________")
